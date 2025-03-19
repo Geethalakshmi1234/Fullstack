e1c7321b0c7e1c33e68c708535182cc369139da5
Geethalakshmi1234 <2322jc22@kgcas.com>
2025-03-19 15:29:19 +0530

    session3

---
+++ session3/LoopingStatements.java
@@ -0,0 +1,47 @@
+package session3;
+
+public class LoopingStatements {
+
+	public static void main(String[] args) {
+
+		//int i = 1;
+
+		
+
+		/*while (i<10) {
+package
+			System.out.println("Final value of i: "+i);
+
+		i++;//i=1(2)
+
+	}
+
+     system.out.println("final value of i:"+i);*/
+
+		
+
+		/*do {
+
+		 system.out.println(" value of i:"+i);
+
+		 --i;
+
+		 } while (i>1);
+
+		 system.out.println("final value of i:"+i);*/
+
+		      	int i;
+
+		          for(i=1;i<=20;i++) {
+
+		        	  System.out.println("value of i: "+i);
+
+		         }
+
+		  System.out.println("The final value of i: "+i);        
+
+     
+
+}
+
+}
---
+++ session3/StringEx.java
@@ -0,0 +1,13 @@
+package session3;
+
+   public class StringEx{
+	   public static void main(String[] args) {
+		String s1 = "Geetha";
+		String s2 = new String("Deepika");
+		char[] c1 = {'J','A','V','A'};
+		String s3 = new String(c1);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+	}
+   }
